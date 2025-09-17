eeba807b7de5116f89e45356bec5e7b91b3c112d
saurabh-23b0131030 <rishi678p@gmail.com>
2025-09-17 15:00:07 +0530

    kiffu

---
+++ Day-3/object.js
@@ -0,0 +1,14 @@
+const car={
+      brand : "Mahindra",
+      model: "XUV 700",
+      year : 2021
+      
+
+}
+console.log(car.model);
+function changemodel(obj){
+    obj.model ="THAR"
+}
+changemodel(car)
+// console.log(car.brand);
+console.log(car.model);
